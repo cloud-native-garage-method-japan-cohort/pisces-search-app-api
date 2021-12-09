882d55d0c414d468929e7f53d89a093ada2144cc
Yukihiro <Yukihiro.Azuma1@ibm.com>
2021-12-09 20:12:02 +0900

    split

--- app.js
+++ app.js
@@ -29,8 +29,44 @@ app.use(cookieParser());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+const resData = {
+  data: [
+      {
+          "text": "コロナ・エキストラ[1]（スペイン語: Corona Extra）とは、モデーロ（Grupo Modelo）が製造するメキシコビールの銘柄。コロナ、コローナと corona の日本語転写は複数あるが、本記事では固有名称以外はアンハイザー・ブッシュ・インベブ・ジャパンが定める「コロナ」と記載する。",
+          "score": 0.92,
+          "concepts": [
+              {
+                  "name":"ビール",
+                  "relevance": 0.88
+              },
+              {
+                  "name": "Youtube",
+                  "relevance": 0.74
+              }
+          ]
+      },
+      {
+          "text": "日本における2019年コロナウイルス感染症の流行状況（にほんにおける2019ねんコロナウイルスかんせんしょうのりゅうこうじょうきょう）では、日本における新型コロナウイルス感染症（COVID-19）の流行状況について述べる。",
+          "score": 0.86,
+          "concepts": [
+              {
+                  "name":"風邪",
+                  "relevance": 0.92
+              },
+              {
+                  "name": "評判",
+                  "relevance": 0.62
+              }
+          ]
+      }
+  ]
+}
+
 app.use('/', indexRouter);
-app.use('/search', searchRouter);
+// app.use('/search', searchRouter);
+app.post('search', (req, res) => {
+  res.json(resData);
+})
 app.use('/mock', mockRouter);
 
 // catch 404 and forward to error handler
