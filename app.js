a84414ab9062c586c159f92482513bc57c17eec6
Yukihiro <Yukihiro.Azuma1@ibm.com>
2021-12-09 17:47:53 +0900

    cors

--- app.js
+++ app.js
@@ -14,6 +14,24 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, access_token'
+  )
+
+  // intercept OPTIONS method
+  if ('OPTIONS' === req.method) {
+    res.send(200)
+  } else {
+    next()
+  }
+}
+app.use(allowCrossDomain)
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
