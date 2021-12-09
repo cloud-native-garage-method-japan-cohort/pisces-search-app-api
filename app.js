2386e2c3a6ec9e7534f273a4dec1db7b0d6b3b27
Yukihiro <Yukihiro.Azuma1@ibm.com>
2021-12-09 20:53:11 +0900

    reverse

--- app.js
+++ app.js
@@ -6,67 +6,35 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const path = require('path');
 
-const app = express();
-const cors = require('cors');
-app.use(cors({
-  "origin": "*",
-  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-  "preflightContinue": false,
-  "optionsSuccessStatus": 200
-}));
-
 const indexRouter = require('./routes/index');
 const searchRouter = require('./routes/search');
 const mockRouter = require('./routes/mock')
 
+const app = express();
+const cors = require('cors');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
+app.use('/', indexRouter);
+app.use('/search', searchRouter);
+app.use('/mock', mockRouter);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-const resData = {
-  data: [
-      {
-          "text": "コロナ・エキストラ[1]（スペイン語: Corona Extra）とは、モデーロ（Grupo Modelo）が製造するメキシコビールの銘柄。コロナ、コローナと corona の日本語転写は複数あるが、本記事では固有名称以外はアンハイザー・ブッシュ・インベブ・ジャパンが定める「コロナ」と記載する。",
-          "score": 0.92,
-          "concepts": [
-              {
-                  "name":"ビール",
-                  "relevance": 0.88
-              },
-              {
-                  "name": "Youtube",
-                  "relevance": 0.74
-              }
-          ]
-      },
-      {
-          "text": "日本における2019年コロナウイルス感染症の流行状況（にほんにおける2019ねんコロナウイルスかんせんしょうのりゅうこうじょうきょう）では、日本における新型コロナウイルス感染症（COVID-19）の流行状況について述べる。",
-          "score": 0.86,
-          "concepts": [
-              {
-                  "name":"風邪",
-                  "relevance": 0.92
-              },
-              {
-                  "name": "評判",
-                  "relevance": 0.62
-              }
-          ]
-      }
-  ]
-}
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
 
 app.use('/', indexRouter);
-// app.use('/search', searchRouter);
-app.post('/search', (req, res) => {
-  res.json(resData);
-})
+app.use('/search', searchRouter);
 app.use('/mock', mockRouter);
 
 // catch 404 and forward to error handler
